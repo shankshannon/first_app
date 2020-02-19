34897b0442190a1fd9d6fc0a4adaa68ee6d0aede
sunshinehope <shankai2018@163.com>
2020-02-19 20:52:05 +0800

    sortedbyvote

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,8 @@
   # GET /topics
   # GET /topics.json
   def index
-    @topics = Topic.all
+    #@topics = Topic.all
+    @topics = Topic.all.sort_by {|topic| topic.votes.count}.reverse
   end
 
   # GET /topics/1
