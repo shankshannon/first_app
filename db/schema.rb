38b0b0b7883d83f631d52c7951dc04eb4000ff63
sunshinehope <shankai2018@163.com>
2020-02-23 11:43:59 +0800

    show groups creator info

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200223012241) do
+ActiveRecord::Schema.define(version: 20200223031725) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "topics", force: :cascade do |t|
